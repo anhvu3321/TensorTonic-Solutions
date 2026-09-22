a9caa7cca2d596e9e45043d11138a1bca4f8e12e
anhvu3321 <anhvu5682@gmail.com>
2026-09-22 08:04:14 +0700

    Solve sample-var-std

---
+++ sample-var-std/sample-var-std.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def sample_var_std(x: list) -> dict:
+    """
+    Returns a dictionary with variance and standard_deviation.
+    """
+    x = np.asarray(x, dtype=float)
+    centered = x - np.mean(x)
+
+    variance = float(np.sum(centered ** 2) / (x.size - 1))
+    deviation = float(np.sqrt(variance))
+
+    return {"variance" : variance, "standard_deviation": deviation}
